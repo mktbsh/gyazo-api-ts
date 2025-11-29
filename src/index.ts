7a4d3b654da401b92322d5373b5d361f50d34aa0
mktbsh <t93.hasebe@gmail.com>
2025-11-29 15:17:13 +0900

    feat: update index.ts to export necessary types and commands

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
-export function fn() {
-	return "Hello, tsdown!";
-}
+export { createGyazoClient } from "./client";
+export type { GyazoCommand } from "./command";
+export * from "./commands/ListImagesCommand";
+export type { GyazoClientConfig } from "./context";
+export * from "./errors";
+export type { GyazoResult } from "./result";
+export * from "./types";
